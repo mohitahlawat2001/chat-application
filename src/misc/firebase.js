5d0211f9ce11b7597de9e470edc76e4b7dca885d
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2023-06-10 16:18:32 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyAh6VdG8676lR-_T2fiwGdA8KKPwmx2nUw",
+    authDomain: "chat-lic.firebaseapp.com",
+    projectId: "chat-lic",
+    storageBucket: "chat-lic.appspot.com",
+    messagingSenderId: "565650128607",
+    appId: "1:565650128607:web:52adf38d24d45ac32ab36a"
+};
+  
+const app =  firebase.initializeApp(config);
