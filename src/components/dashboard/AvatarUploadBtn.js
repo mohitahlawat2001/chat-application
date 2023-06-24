f490401b4c4a023aabb51b8a22491c2d09768272
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2023-06-24 22:26:16 +0530

    update all user reference if data is updated

--- src/components/dashboard/AvatarUploadBtn.js
+++ src/components/dashboard/AvatarUploadBtn.js
@@ -7,6 +7,7 @@ import AvatarEditor from "react-avatar-editor";
 import { useProfile } from "../../context/profile.context";
 import { database, storage } from "../../misc/firebase";
 import ProfileAvatar from "../ProfileAvatar";
+import { getUserUpdates } from "../../misc/helpers";
 
 
 const fileInputTypes = ".jpg, .jpeg, .png";
@@ -65,8 +66,12 @@ const AvatarUploadBtn = () => {
                 cacheControl: `public, max-age=${3600 * 24 * 3}`
             });
             const downloadUrl = await uploadAvatarResult.ref.getDownloadURL();
-            const userAvatarRef = database.ref(`/profiles/${profile.uid}`).child('avatar');
-            userAvatarRef.set(downloadUrl);
+
+            const updates = await getUserUpdates(profile.uid, 'avatar', downloadUrl, database);
+
+            await database.ref().update(updates);
+            // const userAvatarRef = database.ref(`/profiles/${profile.uid}`).child('avatar');
+            // userAvatarRef.set(downloadUrl);
             setIsLoading(false);
             Alert.info('Avatar has been uploaded', 4000);
 
--- src/components/rooms/Roomitems.js
+++ src/components/rooms/Roomitems.js
@@ -1,9 +1,10 @@
 import TimeAgo from "timeago-react"
+import ProfileAvatar from "../ProfileAvatar";
 
 
 const Roomitems = ({ room }) => {
 
-    const { createdAt, name } = room;
+    const { createdAt, name , lastMessage} = room;
 
     return (
         <div>
@@ -11,14 +12,40 @@ const Roomitems = ({ room }) => {
             <div className="d-flex justify-content-between align-items-center">
                 <h4 className="text-disappear">{ name}</h4>
                 <TimeAgo
-                    datetime={new Date(createdAt)}
+                    datetime={
+                        lastMessage ? new Date(lastMessage.createdAt) :
+                        new Date(createdAt)
+                    }
                     className="font-normal text-black-45"
                 />
 
             </div>
 
             <div className="d-flex align-items-center text-black-70 ">
-                <span> no messages yet..</span>
+                {
+                    lastMessage ? (
+                        <>
+                        <div className="d-flex align-items-center">
+                            <div className="d-flex align-items-center mr-1">
+                                <ProfileAvatar
+                                    src={lastMessage.author.avatar}
+                                    name={lastMessage.author.name}
+                                    size="xs"
+                                />
+                            </div>
+                            <div>
+                                <div className="italic">{lastMessage.author.name}</div>
+                                <span>{lastMessage.text}</span>
+                            </div>
+                        </div>
+                        </>
+                    ) :
+                        
+                        
+                                
+                    
+                    <span> no messages yet..</span>
+                }  
             </div>
             
         </div>
