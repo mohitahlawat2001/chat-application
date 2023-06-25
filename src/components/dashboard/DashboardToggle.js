e9d52b173bdf7804e3076b6f4377c149a4eb0eb7
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2023-06-25 12:38:20 +0530

    added real time lostner for a user

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -2,7 +2,8 @@ import { Alert, Button, Drawer, Icon } from "rsuite";
 import { useMediaQuery, useModelState } from "../../misc/custom-hook";
 import Dashboard from ".";
 import { useCallback } from "react";
-import { auth } from "../../misc/firebase";
+import { auth, database } from "../../misc/firebase";
+import { isOfflineForDatabase } from "../../context/profile.context";
 
 const DashboardToggle = () => {
 
@@ -10,11 +11,17 @@ const DashboardToggle = () => {
     const isMobile = useMediaQuery('(max-width: 992px)');
     
     const onSignOut = useCallback(() => {
-        auth.signOut();
 
-        Alert.info('Signed out', 4000);
-
-        close();
+        database.ref(`/status/${auth.currentUser.uid}`)
+            .set(isOfflineForDatabase)
+            .then(() => {
+                auth.signOut();
+                Alert.info('Signed out', 4000);
+                close();
+            }).catch((err) => {
+                Alert.error(err.message, 4000);
+            }
+            );
     }, [close]);
     return (
         <>
--- src/context/profile.context.js
+++ src/context/profile.context.js
@@ -1,5 +1,17 @@
 import { createContext , useContext, useEffect, useState } from 'react';
 import { auth, database } from '../misc/firebase';
+import firebase from 'firebase/app';
+
+export const isOfflineForDatabase = {
+    state: 'offline',
+    last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
+
+const isOnlineForDatabase = {
+    state: 'online',
+    last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
+
 
 const ProfileContext = createContext();
 
@@ -11,9 +23,11 @@ export const ProfileProvider = ({ children }) => {
     useEffect(() => {
 
         let userRef;
+        let userStatusRef;
 
        const authUnsub =  auth.onAuthStateChanged(async (authUser) => {
-            if (authUser) {
+           if (authUser) {
+                userStatusRef = database.ref(`/status/${authUser.uid}`);
 
                 userRef = database.ref(`/profiles/${authUser.uid}`);
                 userRef.on('value', (snapshot) => {
@@ -29,12 +43,31 @@ export const ProfileProvider = ({ children }) => {
                     setProfile(data);     
                     setIsLoading(false);
                 });
+
+                database.ref('.info/connected').on('value', (snapshot) => {
+                    if (snapshot.val() === false) {
+                        return;
+                    }  
+                    userStatusRef.onDisconnect().set(isOfflineForDatabase).then(() => {
+                        userStatusRef.set(isOnlineForDatabase);
+                    }
+                    );
+                });
+
+
+
             } else {
 
                 if (userRef) {
                     userRef.off();
                 }
 
+               if (userStatusRef) {
+                   userStatusRef.off();
+               }
+               
+                database.ref('.info/connected').off();
+               
                 setProfile(null);
                 setIsLoading(false);
             }
@@ -46,6 +79,13 @@ export const ProfileProvider = ({ children }) => {
             if (userRef) {
                 userRef.off();
             }
+
+            if (userStatusRef) {
+                userStatusRef.off();
+            }
+
+            database.ref('.info/connected').off();
+
             
         }
 
