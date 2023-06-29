10b44cb77d057374b4543227fe797092e7de7a96
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2023-06-29 10:16:31 +0530

    added lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import { Switch } from 'react-router-dom';
 
+import React from 'react';
+import { lazy,Suspense } from 'react';
+
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
 
-
-import SignIn from './pages/SignIn';
 import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
 import Home from './pages/Home';
 import { ProfileProvider } from './context/profile.context';
 import { ErrorBoundary } from './components/ErrorBoundary';
 
+const SignIn = lazy(() => import('./pages/SignIn'));
+
 function App() {
   return (
     <ErrorBoundary >
     <ProfileProvider>
       <Switch>
-        <PublicRoute path="/signin">
-          <SignIn/>
+          <PublicRoute path="/signin">
+            <Suspense fallback={<div>Loading...</div>}>
+              <SignIn />
+            </Suspense>
         </PublicRoute>
         <PrivateRoute path="/">
           <Home/>
